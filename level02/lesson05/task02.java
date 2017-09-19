77b915b933568cb49506cca7495449137e9a6ba7
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-19 12:24:59 +0300

    depth of the riches of wisdom and knowledge of God
    О, бездна богатства и премудрости и ведения Божия! Как непостижимы судьбы Его и неисследимы пути Его! 
    O the depth of the riches both of the wisdom and knowledge of God! how unsearchable are his judgments, and his ways past finding out! (Rom 11:33)
    ω 5599 INJ βαθος 899 N-NSN πλουτου 4149 N-GSM και 2532 CONJ σοφιας 4678 N-GSF και 2532 CONJ γνωσεως 1108 N-GSF θεου 2316 N-GSM ως 5613 ADV ανεξερευνητα 419 A-NPN τα 3588 T-NPN κριματα 2917 N-NPN αυτου 846 P-GSM και 2532 CONJ ανεξιχνιαστοι 421 A-NPF αι 3588 T-NPF οδοι 3598 N-NPF αυτου 846 P-GSM

---
+++ level02/lesson05/task02.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level02.lesson05.task02;
+
+//Ибо что говорит Писание? Поверил Авраам Богу, и это вменилось ему в праведность.
+//τι 5101 γαρ 1063 η 3588 γραφη 1124 λεγει 3004 5719 επιστευσεν 4100 5656 δε 1161 αβρααμ 11 τω 3588 θεω 2316 και 2532 ελογισθη 3049 5681 αυτω 846 εις 1519 δικαιοσυνην 1343
+//For what saith the scripture? Abraham believed God, and it was counted unto him for righteousness.
+
+/* If I want your opinion
+Write a program that displays: «If I want your opinion, I'll ask you to fill out the necessary forms.».
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println("If I want your opinion, I'll ask you to fill out the necessary forms.");//add your code here
+    }
+}
