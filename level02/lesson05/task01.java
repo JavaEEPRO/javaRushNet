93a3b45283a7ce27347f5520d9c3897312a808b9
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-19 12:00:02 +0300

    Abraham believed God, its accounted 2righteousness
    Many people are alive only because it’s illegal to shoot them.

---
+++ level02/lesson05/task01.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level02.lesson05.task01;
+
+// Так Авраам поверил Богу, и это вменилось ему в праведность.
+// καθως 2531 αβρααμ 11 επιστευσεν 4100 5656 τω 3588 θεω 2316 και 2532 ελογισθη 3049 5681 αυτω 846 εις 1519 δικαιοσυνην 1343
+// Even as Abraham believed God, and it was accounted to him for righteousness. (Galathians 3:6)
+
+/* Many people are alive
+Write a program that displays 3 times: «Many people are alive only because it's illegal to shoot them.».
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+       for (int i = 0; i < 3; i++){ //add your code here
+       System.out.println("Many people are alive only because it's illegal to shoot them.");
+       }
+    }
+}
+
+
