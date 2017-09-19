6373487c1a4883e31a62be8b6d3cc2265eb1d10e
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-19 17:08:25 +0300

    studying
    Abide in me, and I in you. As the branch cannot bear fruit of itself, except it abide in the vine; no more can ye, except ye abide in me. (John 15:4)

---
+++ level03/lesson06/task05.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level03.lesson06.task05;
+
+//Abide in me, and I in you. As the branch cannot bear fruit of itself, except it abide in the vine;
+//no more can ye, except ye abide in me. (John 15:4)
+
+/* Studying Japanese
+Write a program that displays 日本語
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+       System.out.println("日本語"); //add your code here
+    }
+}
