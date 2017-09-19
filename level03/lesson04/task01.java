b2299e6e84eb7f9bf582896c9f45ba121acc7af7
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-19 15:02:13 +0300

    program that displays your date of birth
    But that the world may know that I love the Father; and as the Father gave me commandment, even so I do. Arise, let us go hence. (John 14:31)

---
+++ level03/lesson04/task01.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level03.lesson04.task01;
+
+//But that the world may know that I love the Father; and as the Father gave me commandment, even so I do. Arise, let us go hence. (John 14:31)
+
+/* Date of birth
+Write a program that displays your date of birth as follows: MAY 1 2012
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println("MAR " + 13 + " " + 1983);//add your code here
+    }
+}
