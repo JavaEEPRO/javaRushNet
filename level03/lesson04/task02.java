4d08057693d5e3cb91704edf1c645fa91e3b59d2
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-19 15:12:57 +0300

    program that displays your name on 5 rows.
    8 Then the king of Syria warred against Israel, and took counsel with his servants, saying, In such and such a place [shall be] my camp.
    
       9 And the man of God sent unto the king of Israel, saying, Beware that thou pass not such a place; for thither the Syrians are come down.
    
       10 And the king of Israel sent to the place which the man of God told him and warned him of, and saved himself there, not once nor twice.
    
       11 Therefore the heart of the king of Syria was sore troubled for this thing; and he called his servants, and said unto them, Will ye not shew me which of us [is] for the king of Israel?
    
       12 And one of his servants said, None, my lord, O king: but Elisha, the prophet that [is] in Israel, telleth the king of Israel the words that thou speakest in thy bedchamber.
    
       13 And he said, Go and spy where he [is], that I may send and fetch him. And it was told him, saying, Behold, [he is] in Dothan.
    
       14 Therefore sent he thither horses, and chariots, and a great host: and they came by night, and compassed the city about.
    
       15 And when the servant of the man of God was risen early, and gone forth, behold, an host compassed the city both with horses and chariots. And his servant said unto him, Alas, my master! how shall we do?
    
       16 And he answered, Fear not: for they that [be] with us [are] more than they that [be] with them.
    
       17 And Elisha prayed, and said, LORD, I pray thee, open his eyes, that he may see. And the LORD opened the eyes of the young man; and he saw: and, behold, the mountain [was] full of horses and chariots of fire round about Elisha.
    
       18 And when they came down to him, Elisha prayed unto the LORD, and said, Smite this people, I pray thee, with blindness. And he smote them with blindness according to the word of Elisha.
    
       19 And Elisha said unto them, This [is] not the way, neither [is] this the city: follow me, and I will bring you to the man whom ye seek. But he led them to Samaria.
    
       20 And it came to pass, when they were come into Samaria, that Elisha said, LORD, open the eyes of these [men], that they may see. And the LORD opened their eyes, and they saw; and, behold, [they were] in the midst of Samaria.
    
       21 And the king of Israel said unto Elisha, when he saw them, My father, shall I smite [them]? shall I smite [them]?
    
       22 And he answered, Thou shalt not smite [them]: wouldest thou smite those whom thou hast taken captive with thy sword and with thy bow? set bread and water before them, that they may eat and drink, and go to their master.
    
       23 And he prepared great provision for them: and when they had eaten and drunk, he sent them away, and they went to their master. So the bands of Syria came no more into the land of Israel. (2Kings, 6)

---
+++ level03/lesson04/task02.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level03.lesson04.task02;
+
+/* My name
+Write a program that displays your name on 5 rows. Each row should contain 10 words (your name) separated by a space.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+    for (int j = 0; j < 5; j++){        
+        for (int i = 0; i < 10; i++){//add your code here
+        System.out.print("servantOfJesus");
+        System.out.print(" ");
+        }
+    System.out.println("");
+    }
+    }
+}
