0b84e68da411c03106c61c423dd8f3ca012103aa
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-20 19:40:34 +0300

    10 numbers
    For I have given unto them the words which thou gavest me; and they have received them, and have known surely that I came out from thee, and they have believed that thou didst send me (John 17:8)

---
+++ level04/lesson10/task01.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level04.lesson10.task01;
+
+//For I have given unto them the words which thou gavest me; and they have received them,
+//and have known surely that I came out from thee, and they have believed that thou didst send me (John 17:8)
+
+
+import java.io.*;
+
+/* 10 numbers
+Write a program that displays numbers from 1 to 10. Use “while” loop.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {   
+       int i = 1;    
+       while(i<11){ //add your code here
+               System.out.println(i);
+               i++;
+       }
+    }
+}
+
