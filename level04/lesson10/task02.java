d60235d07216a9c1ddd16cf54ac1759330b5c4ce
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-20 19:50:44 +0300

    10 numbers in reverse order
    If ye keep my commandments, ye shall abide in my love; even as I have kept my Father's commandments, and abide in his love. (John 15:10)

---
+++ level04/lesson10/task02.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level04.lesson10.task02;
+
+//If ye keep my commandments, ye shall abide in my love; even as I have kept my Father's commandments, 
+//and abide in his love. (John 15:10)
+
+
+import java.io.*;
+
+/* 10 numbers in reverse order
+Write a program that displays numbers from 10 to 1. Use “while” loop.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        int i = 10;
+       while (i > 0){ //add your code here
+       System.out.println(i);
+       i--;
+           
+       }
+
+    }
+}
