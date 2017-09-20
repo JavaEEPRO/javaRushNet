4a968be14a51a415bfef0aa96cf210b2acbf3b2e
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-20 16:53:40 +0300

    Life is good
    так как Ты дал Ему власть над всякою плотью, да всему, что Ты дал Ему, даст Он жизнь вечную.
    As thou hast given him power over all flesh, that he should give eternal life to as many as thou hast given him. (John 17:2)

---
+++ level03/lesson12/home01.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level03.lesson12.home01;
+
+//так как Ты дал Ему власть над всякою плотью, да всему, что Ты дал Ему, даст Он жизнь вечную.
+//As thou hast given him power over all flesh, that he should give eternal life to as many as thou hast given him. (John 17:2)
+
+/* Life is good but good life is even better
+Write a program that displays «Life is good but good life is even better»
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Life is good but good life is even better");//add your code here
+    }
+}
