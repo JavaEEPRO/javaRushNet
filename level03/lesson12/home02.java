d81a3cd6990c77f29aa5c90f2c951c8601bc98a8
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-20 16:35:44 +0300

    just want a huge salary
    He who believes in him is not judged. He who doesn't believe has been judged already, because he has not believed in the name of the one and only Son of God. (John 3:18)

---
+++ level03/lesson12/home02.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level03.lesson12.home02;
+
+//He who believes in him is not judged. He who doesn't believe has been judged already, 
+//because he has not believed in the name of the one and only Son of God. (Jonh 3:18)
+
+/* I don't want to study Java, I just want a huge salary
+Write a program that displays 10 times: «I don't want to study Java, I just want a huge salary»
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+       for (int i = 0; i < 10; i++)
+       {//add your code here
+           System.out.println("I don't want to study Java, I just want a huge salary");
+       }
+    }
+}
