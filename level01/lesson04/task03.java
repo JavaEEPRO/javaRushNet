63a36cdae596c12162bc257d2011a850ad0d5a72
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-21 13:05:49 +0300

    like the way I drive
    No longer do I call you servants, for the servant doesn't know what his lord does. But I have called you friends, for everything that I heard from my Father, I have made known to you. (John 15:16)

---
+++ level01/lesson04/task03.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level01.lesson04.task03;
+
+//No longer do I call you servants, for the servant doesn't know what his lord does. 
+//But I have called you friends, for everything that I heard from my Father, I have made known to you. (John 15:16)
+
+/* Stay off the sidewalk
+Write a program that displays «If you don't like the way I drive, stay off the sidewalk.».
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+       System.out.println("If you don't like the way I drive, stay off the sidewalk."); //add your code here
+    }
+}
