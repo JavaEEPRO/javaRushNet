1bf22ef80189d78eeb65ccf01a546b4fe535b90f
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-21 16:32:26 +0300

    If the world hate you,know that it hated me before
    If the world hate you, ye know that it hated me before it hated you.(John 15:18)

---
+++ level05/lesson05/task01.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level05.lesson05.task01;
+
+//If the world hate you, ye know that it hated me before it hated you.(John 15:18)
+
+/* Create a class Cat
+Create a class Cat. A cat must have its name (name, String), age (age, int), weight (weight, int), and strength (strength, int).
+*/
+
+public class Cat
+{
+    String name;//add your code here
+    int age, weight, strength;
+    public static void main(String[] args){}
+
+}
