db048fa871589f3d0d8d2dbb5dc636308997e595
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-21 13:21:30 +0300

    work for
    You didn't choose me, but I chose you, and appointed you, that you should go and bear fruit, and that your fruit should remain; that whatever you will ask of the Father in my name, he may give it to you. (John 15:16)

---
+++ level04/lesson16/home01.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level04.lesson16.home01;
+
+//You didn't choose me, but I chose you, and appointed you, that you should go and bear fruit, and that your fruit should remain;
+//that whatever you will ask of the Father in my name, he may give it to you. (John 15:16)
+
+/* I'll never work for peanuts
+Write a program that displays hundred times a sentence:
+    «I'll never work for peanuts. Amigo».
+    Use “for” loop.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {//add your code here
+            System.out.println("I'll never work for peanuts. Amigo");
+        }
+    }
+}
