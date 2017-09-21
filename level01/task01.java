9c54d0d4d16aafbbdc888ecf233eb5c743ba17c1
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-21 12:51:48 +0300

    ...who can't speak English.
    And now I am no more in the world, but these are in the world, and I come to thee. Holy Father, keep through thine own name those whom thou hast given me, that they may be one, as we are. (John 17:11)

---
+++ level01/task01.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level01.lesson04.task01;
+
+//And now I am no more in the world, but these are in the world, and I come to thee. 
+//Holy Father, keep through thine own name those whom thou hast given me, that they may be one, as we are. (John 17:11)
+
+/* Blame the guy who can't speak English
+Write a program that displays «If something goes wrong at the office, blame the guy who can't speak English.»
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+      System.out.println("If something goes wrong at the office, blame the guy who can't speak English.");  //add your code here
+    }
+}
