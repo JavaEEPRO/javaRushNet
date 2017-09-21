44121cae70f66fa85b9aab790462bd8b74ceb20a
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-21 10:39:38 +0300

    Even numbers
    Greater love has no one than this, that someone lay down his life for his friends. (John 15:13)

---
+++ level04/lesson13/task01.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level04.lesson13.task01;
+
+//Greater love has no one than this, that someone lay down his life for his friends. (John 15:13)
+
+/* Even numbers
+Write a program that displays even numbers from 1 to 100 separated by spaces or each on a new line. Use “for” loop.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        for (int i = 2; i<101; i+=2){//add your code here
+        System.out.println(i);
+        }
+    }
+}
