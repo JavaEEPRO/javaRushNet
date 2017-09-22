857eadf31e5a252bbcb65ebb3dd997eafae68dab
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-22 20:12:21 +0300

    total "they may be perfected into one"
    I in them, and you in me, that they may be perfected into one; that the world may know that you sent me, and loved them, even as you loved me.(John 17:23)

---
+++ level05/lesson12/home05.java
@@ -0,0 +1,6 @@
+
+
+//I in them, and you in me, that they may be perfected into one; 
+//that the world may know that you sent me, and loved them, even as you loved me.(John 17:23)
+
+total "they may be perfected into one"
