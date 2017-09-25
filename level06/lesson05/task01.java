55542b9c1250321a0dcf5d94d4c6da0b46e6a1c4
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-25 20:13:09 +0300

    love with which you loved me may be in them
    finalize()
    I made known to them your name, and will make it known; that the love with which you loved me may be in them, and I in them." (John 17:26)

---
+++ level06/lesson05/task01.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level06.lesson05.task01;
+
+//I made known to them your name, and will make it known; that the love with which you loved me may be in them, and I in them." (John 17:26)
+
+/* finalize() method of Cat class
+Write a method protected void finalize() throws Throwable in the class Cat.
+*/
+
+public class Cat
+{
+   protected void finalize() throws Throwable{ //add your code here
+   }
+}
+
