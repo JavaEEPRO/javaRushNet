2c7d059a60b679fd47716d127906d156afa2fc67
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-26 12:26:47 +0300

    Jesus of Nazareth
    Again therefore he asked them, "Who are you looking for?" They said, "Jesus of Nazareth." (John 18:7)

---
+++ level06/lesson11/home07.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level06.lesson11.home07;
+
+//Again therefore he asked them, "Who are you looking for?" They said, "Jesus of Nazareth." (John 18:7)
+
+import java.util.ArrayList;
+
+/* Three static variables name
+Write 3 public static variables: String Solution.name, String Cat.name, String Dog.name
+*/
+
+public class Solution
+{
+    public static String name;
+    public static class Cat {
+        public static String name;
+    }
+
+    public static class Dog {
+        public static String name;
+    }
+}
