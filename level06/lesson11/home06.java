c6576eddfae9559be26024fb5fbfed3b0271e60a
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-26 12:22:24 +0300

    I tell you the truth
    Nevertheless I tell you the truth; It is expedient for you that I go away: for if I go not away, the Comforter will not come unto you; but if I depart, I will send him unto you.(John 16:7)

---
+++ level06/lesson11/home06.java
@@ -0,0 +1,25 @@
+
+
+//Nevertheless I tell you the truth; It is expedient for you that I go away: 
+//for if I go not away, the Comforter will not come unto you; but if I depart, I will send him unto you.(John 16:7)
+
+
+package com.javarush.test.level06.lesson11.home06;
+
+/* Kiss
+Write a class named Kiss. Create an object of this class, then display it to the screen.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new Kiss());
+    }
+
+    public static class Kiss
+    {
+
+    }
+
+}
