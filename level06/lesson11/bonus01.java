fb09a7da36e8026f0c76c464912e7770b5c4ebf3
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-09-26 13:45:22 +0300

    max (fix it)
    And when he is come, he will reprove the world of sin, and of righteousness, and of judgment (John 16:8)

---
+++ level06/lesson11/bonus01.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level06.lesson11.bonus01;
+
+//And when he is come, he will reprove the world of sin, and of righteousness, and of judgment (John 16:8)
+
+/* Program doesn’t compile and run. Fix it.
+Task: The program should display the maximum of two numbers that are entered from the keyboard in a following way: «Max is 25».
+*/
+import java.io.*;
+public class Solution
+{
+    public static int max = 100;
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
+
+        String max = "Max is ";
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        max += a > b ? a : b;
+
+        System.out.println(max);
+    }
+}
