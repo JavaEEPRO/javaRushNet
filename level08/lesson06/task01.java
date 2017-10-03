85c190788683df07b705422215e49a9d338a6b3a
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-03 16:55:20 +0300

    Create two lists LinkedList & ArrayList.
    And Annas sent him bound to Caiphas the high priest.(John 18:24)

---
+++ level08/lesson06/task01.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level08.lesson06.task01;
+
+//And Annas sent him bound to Caiphas the high priest.(John 18:24)
+
+import java.util.*;
+
+/* Create two lists LinkedList & ArrayList.
+Create two lists: LinkedList and ArrayList.
+*/
+
+public class Solution
+{
+    public static Object createArrayList()
+    {
+       ArrayList<String> list = new ArrayList<String>(); //add your code here
+       return list;
+    }
+
+    public static Object createLinkedList()
+    {
+       LinkedList<String> listik = new LinkedList<String>(); //add your code here
+       return listik;
+    }
+    
+    public static void main(String [] args){}
+}
+
