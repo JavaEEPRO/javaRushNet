77b5307e5d08b5bbca72462bd9a3c64d69a8e4a4
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-10 10:38:46 +0300

    return a result - its stack trace depth
    Pilate answered, "I'm not a Jew, am I? Your own nation and the chief priests delivered you to me. What have you done?" (John 18:35)

--- level09/lesson02/task05.java
+++ level09/lesson02/task05.java
@@ -1,7 +1,9 @@
 package com.javarush.test.level09.lesson02.task05;
 
+
 //Pilate answered, "I'm not a Jew, am I? 
 //Your own nation and the chief priests delivered you to me. What have you done?" (John 18:35)
+
 
 /* The method should return a result - its stack trace depth
 Write a method that displays and returns its stack trace depth. Stack trace depth is the number of its methods (the number of elements in the list).
